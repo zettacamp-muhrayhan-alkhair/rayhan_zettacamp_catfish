74a0c8cd613a5f5dd6fbf5a20946c29fb804f6ba
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-11 09:49:20 +0700

    - js day 2

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// let inputBooks = prompt('Enter the number of books you want to buy :');
+let inputBooks;
+let stockBooks = 10;
+let disc = 50 / 100;
+let priceBook = 10000;
+let totalPrice;
+let tax = 10 / 100;
+function bookPurchase(inputBooks) {
+    for ()
+
+  totalPrice = priceBook * inputBooks;
+  totalPrice = totalPrice - totalPrice * disc;
+  totalPrice = totalPrice + totalPrice * tax;
+  console.log(totalPrice);
+}
+
+bookPurchase(prompt('Enter the number of books you want to buy :'));
