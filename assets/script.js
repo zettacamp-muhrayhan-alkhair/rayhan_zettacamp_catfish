472a565dbdf85cc61fd515b80a13f5defe898ee1
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-11 10:51:16 +0700

    - js day 2

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,61 @@
-// let inputBooks = prompt('Enter the number of books you want to buy :');
+let wantToBuy = confirm('Want to buy in my store?');
 let inputBooks;
-let stockBooks = 10;
 let disc = 50 / 100;
+let stockBooks = 10;
 let priceBook = 10000;
 let totalPrice;
 let tax = 10 / 100;
+// function bookPurchase(inputBooks) {
+//   totalPrice = priceBook * inputBooks;
+//   totalPrice = totalPrice - totalPrice * disc;
+//   totalPrice = totalPrice + totalPrice * tax;
+//   console.log(`You buy ${inputBooks} books`);
+//   console.log(totalPrice);
+// }
+
 function bookPurchase(inputBooks) {
-    for ()
+  if (stockBooks > 0 && stockBooks > inputBooks) {
+    totalPrice = priceBook * inputBooks;
+    totalPrice = totalPrice - totalPrice * disc;
+    totalPrice = totalPrice + totalPrice * tax;
+
+    console.log(`You buy ${inputBooks} books`);
+    console.log(totalPrice);
+  }
 
   totalPrice = priceBook * inputBooks;
   totalPrice = totalPrice - totalPrice * disc;
   totalPrice = totalPrice + totalPrice * tax;
+  stockBooks = stockBooks - inputBooks;
+  console.log(`You buy ${inputBooks} books`);
   console.log(totalPrice);
 }
 
-bookPurchase(prompt('Enter the number of books you want to buy :'));
+// if (wantToBuy == true) {
+//   bookPurchase(prompt('Enter the number of books you want to buy :'));
+// } else {
+//   console.log('Thank you for wasting my time!!!');
+// }
+
+// if (wantToBuy == true) {
+//     for
+//   bookPurchase(prompt('Enter the number of books you want to buy :'));
+// } else {
+//   console.log('Thank you for wasting my time!!!');
+// }
+
+// while (wantToBuy) {
+//   bookPurchase(prompt(`Enter the number of books you want to buy : (Total Stocks = ${stockBooks})`));
+//   stockBooks -= inputBooks;
+//   wantToBuy = confirm('Want to buy in my store?');
+// }
+
+// if (wantToBuy) {
+//   while (stockBooks > 0) {
+//     bookPurchase(prompt(`Enter the number of books you want to buy : (Total Stocks = ${stockBooks})`));
+//     stockBooks -= inputBooks;
+//   }
+//   wantToBuy = confirm('Want to buy in my store again?');
+// } else {
+//   console.log('Thank you for wasting my time!!!');
+// }
