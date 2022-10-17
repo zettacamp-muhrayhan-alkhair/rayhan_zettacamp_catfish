31bb0e503272f7423cefa7f1df92be2244c5b5b2
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-17 10:24:57 +0700

    - ts day 1

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 const sentence: string = 'Learning Typescript is different than Javascript';
 
-const getWord = (sentence: string, start: number, end: number): string => {
+// const getWord = (sentence: string, start: number, end: number): string => {
+//   return sentence.slice(start, end);
+// };
+function getWord(sentence: string, start: number, end: number): string {
   return sentence.slice(start, end);
-};
+}
 
 console.log(getWord(sentence, 9, 19));
