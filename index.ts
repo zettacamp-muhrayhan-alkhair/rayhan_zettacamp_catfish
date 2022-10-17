b98a8ae41201c05a8b897541362b33fdc4dcf46d
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-17 10:04:08 +0700

    - ts day 2

---
+++ index.ts
@@ -0,0 +1,27 @@
+let firstCase: Array<string | number> = [1, 'data', 3, 'result'];
+let firstFinalResult: string = '';
+
+function firstResult(): any {
+  for (let i: number = 0; i < firstCase.length; i++) {
+    firstFinalResult += firstCase[i];
+    firstFinalResult += ' ';
+  }
+  return firstFinalResult;
+}
+
+console.log(firstResult());
+console.log(typeof firstResult());
+
+let secondCase: Array<string> = ['Bejo', 'has', '4', 'sport', 'car'];
+let finalSecondResult: string = '';
+
+function secondResult(): any {
+  for (let i: number = 0; i < secondCase.length; i++) {
+    finalSecondResult += secondCase[i];
+    finalSecondResult += ' ';
+  }
+  return finalSecondResult;
+}
+
+console.log(secondResult());
+console.log(typeof secondResult());
