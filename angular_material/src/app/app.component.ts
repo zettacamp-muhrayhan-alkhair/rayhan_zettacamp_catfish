b6a523d543f1672fb4814c91d495d63254d1a92d
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-24 15:04:57 +0700

    angular day 5 first task

--- angular_material/src/app/app.component.ts
+++ angular_material/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular_material';
+  // selectedStudent:
+  constructor(private data: DataService) {}
+
+  students: {}[] = [];
+  ngOnInit(): void {
+    this.students = this.data.students;
+    // this.data.selectedStudent$.subscribe((student) => {
+    //   this.selectedStudent = student;
+    // });
+  }
 }
