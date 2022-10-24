7c8968b01fc51b3f0bb5e13f4286d2f530bf2fa9
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-24 23:59:17 +0700

    update angular master

--- angular_materiall/karma.conf.js
+++ angular_materiall/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular_material'),
+      dir: require('path').join(__dirname, './coverage/angular_materiall'),
       subdir: '.',
       reporters: [
         { type: 'html' },
