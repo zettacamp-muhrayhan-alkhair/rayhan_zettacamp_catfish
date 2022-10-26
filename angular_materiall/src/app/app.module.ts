738e63fe7bc701c7456d27df975464fcc2b53905
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-26 10:39:46 +0700

    angular master for angular daily task

--- angular_materiall/src/app/app.module.ts
+++ angular_materiall/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Import Angular Material Module
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
