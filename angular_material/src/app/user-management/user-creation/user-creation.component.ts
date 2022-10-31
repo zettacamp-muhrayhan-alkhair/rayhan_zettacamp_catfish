04de0d25ef8633a677d72b740757faa5126684b6
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-10-31 15:15:32 +0700

    angular day 10 done

--- angular_material/src/app/user-management/user-creation/user-creation.component.ts
+++ angular_material/src/app/user-management/user-creation/user-creation.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  FormArray,
-  FormBuilder,
-  Validators,
-} from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserManagementService } from '../user-management.service';
 import { first } from 'rxjs';
@@ -22,9 +16,19 @@ import Swal from 'sweetalert2';
 export class UserCreationComponent implements OnInit {
   // userForm: any;
   userForm: any = this.fb.group({
-    _id: this.fb.control(null, Validators.required),
-    name: this.fb.control(null, Validators.required),
-    age: this.fb.control(null, Validators.required),
+    _id: this.fb.control(null, [
+      Validators.required,
+      Validators.pattern('^[0-9]+$'),
+    ]),
+    name: this.fb.control(null, [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z]+$'),
+    ]),
+    age: this.fb.control(null, [
+      Validators.required,
+      Validators.min(12),
+      Validators.pattern('^[0-9]+$'),
+    ]),
     gender: this.fb.control(null, Validators.required),
     email: this.fb.control(null, [Validators.required, Validators.email]),
     position: this.fb.control(null, Validators.required),
@@ -61,45 +65,72 @@ export class UserCreationComponent implements OnInit {
     } else {
       this.addAddresses();
     }
-  }
 
-  setFormValues(user: any) {
-    this.userForm.setValue(user);
+    //subscribe to prevent special char and number in name input field
+    this.userForm.get('name').valueChanges.subscribe((inputName: any) => {
+      const specialChar = /[^a-z|\s]/i;
+
+      let finalName: any = inputName;
+      finalName = finalName.replace(specialChar, '');
+      this.userForm.get('name').patchValue(finalName, { emitEvent: false });
+    });
   }
 
   onSubmit() {
+    // if isEdit truthy
     if (this.isEdit) {
-      this.userManagementService.updateUser(this.userForm.value);
-      Swal.fire('User Updated', 'You just updated the user', 'success');
-      // Make form null again
-      this.userForm.reset();
-      // ROute to user list
-      this.router.navigate(['/user-list']);
-    } else {
-      this.userManagementService.addUserToData(this.userForm.value);
-      Swal.fire('User Added', 'You just added new user', 'success');
-      // Make form null again
-      this.userForm.reset();
-      // ROute to user list
-      this.router.navigate(['/user-list']);
+      // if userForm valid truthy
+      if (this.userForm.valid) {
+        this.userManagementService.updateUser(this.userForm.value);
+        Swal.fire('User Updated', 'You just updated the user', 'success');
+        this.router.navigate(['/user-list']);
+      }
+      // if userForm falsy
+      else {
+        Swal.fire(
+          'User Updated Error',
+          'Please input the valid data',
+          'warning'
+        );
+      }
+    }
+    // if isEdit falsy
+    else {
+      // if userForm valid truthy
+      if (this.userForm.valid) {
+        this.userManagementService.addUserToData(this.userForm.value);
+        Swal.fire('User Updated', 'You just updated the user', 'success');
+        this.router.navigate(['/user-list']);
+      }
+      // if userForm valid falsy
+      else {
+        Swal.fire('User Added Error', 'Please input the valid data', 'warning');
+      }
     }
   }
 
+  // setLanguage method
   setLanguage(lang: string) {
     this.selectedLang = lang;
     this.translateService.use(lang);
   }
 
+  // get addresses as a formarray
   get addresses(): FormArray {
     return this.userForm.get('addresses') as FormArray;
   }
 
   newAddresses(): FormGroup {
     return this.fb.group({
-      address: this.fb.control(null, Validators.required),
-      zipCode: this.fb.control(null, Validators.required),
-      city: this.fb.control(null, Validators.required),
-      country: this.fb.control(null, Validators.required),
+      address: this.fb.control(null, [Validators.required]),
+      zipCode: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(5),
+        Validators.pattern('^[0-9]+$'),
+      ]),
+      city: this.fb.control(null, [Validators.required]),
+      country: this.fb.control(null, [Validators.required]),
     });
   }
 
