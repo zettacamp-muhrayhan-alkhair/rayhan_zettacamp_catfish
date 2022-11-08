a243c28fdf3d50207e8db808c03b8ebbcb6b82b4
rayhanalkhair <muh.rayhanalkhair@gmail.com>
2022-11-08 10:24:51 +0700

    [Init] Angular Apollo Master

--- angular_material/src/app/app.module.ts
+++ angular_material/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 // Module
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AngularMaterialModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, AngularMaterialModule, BrowserAnimationsModule, GraphQLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
