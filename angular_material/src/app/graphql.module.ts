b4bc5bc1ee907bbd37962cf5a4c5ef52436d8687
zettacamp-muhrayhan-alkhair <114376961+zettacamp-muhrayhan-alkhair@users.noreply.github.com>
2022-12-07 15:24:50 +0700

    Update graphql.module.ts

--- angular_material/src/app/graphql.module.ts
+++ angular_material/src/app/graphql.module.ts
@@ -7,7 +7,7 @@ import {
 } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 
-const uri = 'https://cf78-103-236-192-220.ap.ngrok.io/graphql';
+const uri = 'https://e9f1-103-236-192-220.ap.ngrok.io/graphql';
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   const http = httpLink.create({ uri: uri });
